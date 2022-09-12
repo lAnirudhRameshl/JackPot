e3f158a4c2b0fa83e9452cb85430c42cd0f5c8e8
Aadarsh <a686102@fmr.com>
2022-09-13 01:16:48 +0530

    BUY and SELL Buttons & B/S Panel

--- JackPot/src/app/app-routing.module.ts
+++ JackPot/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { BuySellPanelComponent } from './components/molecules/buy-sell-panel/buy-sell-panel.component';
+import { ButtonTradeComponent } from './components/atoms/button-trade/button-trade.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'aadarsh', component: BuySellPanelComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
