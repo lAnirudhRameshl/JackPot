773104256fd4024736f6537521f02b35ee898416
SwathikaBalamurugan <easycp100@gmail.com>
2022-10-12 11:58:22 +0530

    Skeleton for JDBC Connection

---
+++ Jackpot Backend/src/main/java/com/fidelity/integration/DataSource.java
@@ -0,0 +1,24 @@
+package com.fidelity.integration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class DataSource {
+   private static Connection connection = null;
+
+    private DataSource() {}
+
+    public static Connection getConnection() throws SQLException {
+        if(connection == null){
+            ResourceBundle resourceBundle = ResourceBundle.getBundle("db.properties");
+            String url = resourceBundle.getString("db.url");
+            String username=resourceBundle.getString("db.username");
+            String psswd=resourceBundle.getString("db.password");
+
+            connection= DriverManager.getConnection(url,username,psswd);
+        }
+        return connection;
+    }
+}
