70615ff959d3a4405e0281399b86ddaff38576d2
Aadarsh <a686102@fmr.com>
2022-11-07 19:01:03 +0530

    Accounts API donezo

--- Backend/jackpot/src/main/java/com/fidelity/jackpot/controller/AccountController.java
+++ Backend/jackpot/src/main/java/com/fidelity/jackpot/controller/AccountController.java
@@ -3,8 +3,12 @@ package com.fidelity.jackpot.controller;
 import com.fidelity.jackpot.model.AccountType;
 import com.fidelity.jackpot.model.User;
 import com.fidelity.jackpot.model.UserAccount;
+import com.fidelity.jackpot.payload.AddAccountRequest;
+import com.fidelity.jackpot.payload.UpdateAccountRequest;
 import com.fidelity.jackpot.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -29,6 +33,38 @@ public class AccountController {
         return accountService.getUserAccountByUserId(userId);
 
     }
+
+    @PostMapping
+    public ResponseEntity<?> addUserAccountByUserId(@RequestBody AddAccountRequest addAcc){
+
+        ResponseEntity response = null;
+
+        System.out.println("POST BODY => "+addAcc);
+
+        try{
+            response =  new ResponseEntity(accountService.addUserAccountByUserId(addAcc), HttpStatus.OK) ;
+        }catch (Exception e) {
+            response = new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return response;
+
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUserAccountMarginByUserAccountId(@RequestBody UpdateAccountRequest updAcc){
+        ResponseEntity response = null;
+
+        System.out.println("POST BODY => "+updAcc);
+
+        try{
+            response =  new ResponseEntity(accountService.updateUserAccountMarginByUserAccountId(updAcc), HttpStatus.OK) ;
+        }catch (Exception e) {
+            response = new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return response;
+
+    }
+
 
 
 }
--- Backend/jackpot/src/main/java/com/fidelity/jackpot/service/AccountService.java
+++ Backend/jackpot/src/main/java/com/fidelity/jackpot/service/AccountService.java
@@ -3,15 +3,15 @@ package com.fidelity.jackpot.service;
 import com.fidelity.jackpot.model.AccountType;
 import com.fidelity.jackpot.model.User;
 import com.fidelity.jackpot.model.UserAccount;
+import com.fidelity.jackpot.payload.AddAccountRequest;
+import com.fidelity.jackpot.payload.UpdateAccountRequest;
 import com.fidelity.jackpot.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-import java.math.BigDecimal;
 
 @Service
 public class AccountService  {
@@ -25,8 +25,33 @@ public class AccountService  {
 
     }
 
-    public UserAccount addUserAccountByUserId(){return null;}
+    @Transactional
+    public UserAccount addUserAccountByUserId(AddAccountRequest addAcc){
 
-    public UserAccount updateUserAccountByUserId(){return null;}
+         UserAccount UA_from_req = new UserAccount();;
+
+        //System.out.println(addAcc.toString());
+        UA_from_req.setUserAccountId(addAcc.getUserAccountId());
+        UA_from_req.setMarginAvailable(addAcc.getMarginAvailable());
+        UA_from_req.setMarginUsed(addAcc.getMarginUsed());
+
+        UA_from_req.setUser(new User());
+        UA_from_req.setAccountType(new AccountType());
+        UA_from_req.getAccountType().setAccountTypeId(addAcc.getAccountTypeId());
+        UA_from_req.getUser().setUserId(addAcc.getUserId());
+
+        return userAccountRepository.save(UA_from_req);
+    }
+
+    @Transactional
+    public UserAccount updateUserAccountMarginByUserAccountId(UpdateAccountRequest uar){
+
+        UserAccount acc2update = userAccountRepository.findById(uar.getUserAccountId()).get();
+        acc2update.setMarginAvailable(uar.getMarginAvailable());
+        acc2update.setMarginUsed(uar.getMarginUsed());
+
+        return userAccountRepository.save(acc2update);
+
+    }
     
 }
