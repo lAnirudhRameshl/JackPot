67866c987c01679538b3c5d9f60d5dce04b7acbb
Anirudh Ramesh <anirudhramesh0@gmail.com>
2022-11-08 14:09:14 +0530

    developed JWT and OAuth2

---
+++ Backend/jackpot/src/main/java/com/fidelity/jackpot/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.fidelity.jackpot.controller;
+
+import com.fidelity.jackpot.payload.LoginRequest;
+import com.fidelity.jackpot.payload.LoginResponse;
+import com.fidelity.jackpot.security.JwtHelper;
+import com.fidelity.jackpot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jackpot/api/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        ResponseEntity<?> response = null;
+
+
+
+        return response;
+    }
+}
--- Backend/jackpot/src/main/java/com/fidelity/jackpot/service/UserService.java
+++ Backend/jackpot/src/main/java/com/fidelity/jackpot/service/UserService.java
@@ -1,25 +1,30 @@
 package com.fidelity.jackpot.service;
 
+import com.fidelity.jackpot.exception.ResourceNotFoundException;
+import com.fidelity.jackpot.exception.UserException;
 import com.fidelity.jackpot.model.User;
+import com.fidelity.jackpot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-    List<User> getAllUsers (){
-        return null;
-    }
-    User getByUserId (){
-        return null;
-    }
-    User addUser (User user){
-        return null;
-    }
-    User updateUser (User user,Long userId){
-        return null;
-    }
-    void deleteUser (Long userId){
-//        return null;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean login(String email, String password) {
+        boolean isLoggedIn = false;
+
+        try {
+            User user = userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Invalid credentials"));
+
+        } catch (Exception e) {
+            throw new UserException(e.getMessage());
+        }
+
+        return isLoggedIn;
     }
 }
