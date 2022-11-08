8bee46adde410c46365a31b005bdf2392cabebde
Shreyanshi Srivastava <shri.srvstv@gmail.com>
2022-11-08 14:44:19 +0530

    trade history completed

--- Backend/jackpot/src/main/java/com/fidelity/jackpot/controller/TradeHistoryController.java
+++ Backend/jackpot/src/main/java/com/fidelity/jackpot/controller/TradeHistoryController.java
@@ -2,6 +2,7 @@ package com.fidelity.jackpot.controller;
 
 import com.fidelity.jackpot.model.TradeHistory;
 import com.fidelity.jackpot.payload.TradeHistoryDto;
+import com.fidelity.jackpot.payload.TradeHistoryResponse;
 import com.fidelity.jackpot.service.TradeHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,25 +11,26 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/trade-history")
+@RequestMapping("/jackpot/api/trade-history")
 @RestController
 public class TradeHistoryController {
 
     @Autowired
     private TradeHistoryService tradeHistoryService;
 
-    @GetMapping
-    public ResponseEntity<List<TradeHistory>> getaALL(){
-        List<TradeHistory> tradeHistories = tradeHistoryService.getAll();
-
-        ResponseEntity<List<TradeHistory>> responseEntity;
-        responseEntity = ResponseEntity.ok(tradeHistories);
-        return responseEntity;
-    }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/{userId}")
-    public List<TradeHistory> getTradeHistoryByUserUserId(@PathVariable Long userId){
-        return tradeHistoryService.getTradeHistoryByUserUserId(userId);
+    public ResponseEntity<List<TradeHistoryResponse>> getTradeHistoryByUserUserId(@PathVariable Long userId){
+        ResponseEntity response = null;
+
+        try {
+            List<TradeHistoryResponse> responseBody = tradeHistoryService.getTradeHistoryByUserUserId(userId);
+            response = new ResponseEntity(responseBody, HttpStatus.OK);
+        } catch (Exception e) {
+            response = new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return response;
     }
 
     @PostMapping
--- Backend/jackpot/src/main/java/com/fidelity/jackpot/service/TradeHistoryService.java
+++ Backend/jackpot/src/main/java/com/fidelity/jackpot/service/TradeHistoryService.java
@@ -2,10 +2,12 @@ package com.fidelity.jackpot.service;
 
 import com.fidelity.jackpot.model.TradeHistory;
 import com.fidelity.jackpot.payload.TradeHistoryDto;
+import com.fidelity.jackpot.payload.TradeHistoryResponse;
 import com.fidelity.jackpot.repository.TradeHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,14 +16,22 @@ public class TradeHistoryService
     @Autowired
     private TradeHistoryRepository tradeHistoryRepository;
 
-    public List<TradeHistory> getAll(){
-        return tradeHistoryRepository.findAll();
-    }
+    public List<TradeHistoryResponse> getTradeHistoryByUserUserId (Long userId){
 
-    public List<TradeHistory> getTradeHistoryByUserUserId (Long userId){
+        List<TradeHistory> tradeHistories = tradeHistoryRepository.findByUserUserId(userId);
+        List<TradeHistoryResponse> tradeHistoryResponses = new ArrayList<>();
+        for (TradeHistory tradeHistory: tradeHistories ){
+            TradeHistoryResponse tradeHistoryResponse = new TradeHistoryResponse();
+            tradeHistoryResponse.setFund(tradeHistory.getFundName());
+            tradeHistoryResponse.setQuantity(tradeHistory.getQuantity());
+            tradeHistoryResponse.setPrice(tradeHistory.getPrice());
+            tradeHistoryResponse.setAccount(tradeHistory.getAccountType().getAccountTypeName());
+            tradeHistoryResponse.setDate(tradeHistory.getTransactionDate());
+            tradeHistoryResponse.setAsset(tradeHistory.getAssetClass().getAssetClassName());
 
-        return tradeHistoryRepository.findByUserUserId(userId);
-
+            tradeHistoryResponses.add(tradeHistoryResponse);
+        }
+        return tradeHistoryResponses;
     }
     public Integer insertTradeHistoryByUserId (TradeHistoryDto tradeHistoryDto){
          return tradeHistoryRepository.insertTradeHistoryByUserId(tradeHistoryDto);
